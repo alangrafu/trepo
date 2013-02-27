8b966c983c5c7535464ade1e8039943326d8255d
Alvaro Graves <alvaro@graves.cl>
2013-02-26 23:18:12 -0800

    primero tar, lueg mueve

--- bin/createArchive.sh
+++ bin/createArchive.sh
@@ -3,4 +3,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd  $DIR/../upload
-tar zcvf $DIR/../archive.tar.gz *
+tar cvf $DIR/../_archive.tar *.zip
+mv $DIR/../_archive.tar $DIR/../archive.tar 
