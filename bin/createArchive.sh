0f50f79795081a8f44457c0bc8938e0c3b7e2b43
Alvaro Graves <alvaro@graves.cl>
2013-03-01 00:40:56 -0800

    agregado contador de usuarios que han aportado

--- bin/createArchive.sh
+++ bin/createArchive.sh
@@ -3,5 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd  $DIR/../upload
+TOTAL=`ls *.zip|wc -l`
 tar cvf $DIR/../_archive.tar *.zip CONTRIBUTORS LICENSE README
 mv $DIR/../_archive.tar $DIR/../archive.tar 
+echo "Currently, $TOTAL people has donated their tweets" > $DIR/../currentNumber.txt 
